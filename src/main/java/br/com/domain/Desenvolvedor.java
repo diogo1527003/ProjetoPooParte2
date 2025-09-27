f2d9a513183f0fd0ed75e0a38065910c8da9ff71
diogo1527003 <diogopiresgo@gmail.com>
2025-09-26 21:52:00 -0300

    aula projeto poo parte2

---
+++ src/main/java/br/com/domain/Desenvolvedor.java
@@ -0,0 +1,28 @@
+package br.com.domain;
+
+public class Desenvolvedor extends Funcionario{
+
+    private int horasExtra;
+    private Double valorHoraExtra;
+
+    public Desenvolvedor(String nome, double salarioBase, int horasExtra, Double valorHoraExtra, String matricula) {
+        super(nome, matricula, salarioBase);
+        this.horasExtra = horasExtra;
+        this.valorHoraExtra = valorHoraExtra;
+
+
+    }
+
+
+
+
+    @Override
+    public double calcularSalario() {
+         return salarioBase +(valorHoraExtra * horasExtra);
+    }
+    public void registrarHorasExtras(int horasExtra){
+        this.horasExtra = horasExtra;
+    }
+
+}
+
---
+++ src/main/java/br/com/domain/SistemaRH.java
@@ -0,0 +1,12 @@
+package br.com.domain;
+
+public class SistemaRH {
+    public static void main(String[] args){
+
+        // Criando Funcionarios - POLOMORFISMO
+        Funcionario[] funcionario = new Funcionario[3];
+
+
+    }
+
+}
